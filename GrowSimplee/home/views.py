15e55d08ea373878530b701ca1972c1580e22a29
Parth Jain <parthljain2002@gmail.com>
2023-01-28 12:22:51 +0530

    Added the routing algorithm

--- GrowSimplee/home/views.py
+++ GrowSimplee/home/views.py
@@ -5,6 +5,8 @@ from django.core.files import File
 import pandas as pd
 import requests
 from geopy.geocoders import Nominatim
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 import urllib.parse
 
 # Create your views here.
@@ -49,6 +51,8 @@ import urllib.parse
 
 # Data needed to be passed : ‘Demands’, ‘Time windows’, ‘Vehicle capacities’, ‘Distance matrix’ and ‘Time matrix’
 
+
+driver_routes = []
 
 def index(request):
     response = {}
@@ -98,11 +102,11 @@ def dispatch_addresses(request):
 
 
 def driver_route(request):
+    index = request.GET.get('index')
     response = {}
     response['status'] = 'OK'
     response['message'] = 'This is the driver route page'
-    # For now, hard coded the route
-    response['route'] = [[28,77],[30,77],[29,78]]
+    response['route'] = driver_routes[index]
     return JsonResponse(response)
 
 def admin_routes(request):
@@ -110,7 +114,7 @@ def admin_routes(request):
     response['status'] = 'OK'
     response['message'] = 'This is the admin routes page'
     # For now, hard coded the routes
-    response['routes'] = [[[28,77],[30,77]],[[28,78],[30,78],[32,78]]]
+    response['routes'] = driver_routes
     return JsonResponse(response)
 
 def waypoint_to_coord(query):
@@ -121,6 +125,172 @@ def waypoint_to_coord(query):
     data = response.json()
     print(data)
     return data['data'][0]['latitude'], data['data'][0]['longitude']
+
+
+
+def create_data_model():
+    """Stores the data for the problem."""
+
+    # In this we need to create time matrix using the DISTANCE API
+    # Time window, vehicle capacity, demands, num_vehicles will be provided in the data
+    data = {}
+    data['time_matrix'] = [
+                [0, 6, 9, 8, 7, 3, 6, 2, 3, 2, 6, 6, 4, 4, 5, 9, 7],
+                [6, 0, 8, 3, 2, 6, 8, 4, 8, 8, 13, 7, 5, 8, 12, 10, 14],
+                [9, 8, 0, 11, 10, 6, 3, 9, 5, 8, 4, 15, 14, 13, 9, 18, 9],
+                [8, 3, 11, 0, 1, 7, 10, 6, 10, 10, 14, 6, 7, 9, 14, 6, 16],
+                [7, 2, 10, 1, 0, 6, 9, 4, 8, 9, 13, 4, 6, 8, 12, 8, 14],
+                [3, 6, 6, 7, 6, 0, 2, 3, 2, 2, 7, 9, 7, 7, 6, 12, 8],
+                [6, 8, 3, 10, 9, 2, 0, 6, 2, 5, 4, 12, 10, 10, 6, 15, 5],
+                [2, 4, 9, 6, 4, 3, 6, 0, 4, 4, 8, 5, 4, 3, 7, 8, 10],
+                [3, 8, 5, 10, 8, 2, 2, 4, 0, 3, 4, 9, 8, 7, 3, 13, 6],
+                [2, 8, 8, 10, 9, 2, 5, 4, 3, 0, 4, 6, 5, 4, 3, 9, 5],
+                [6, 13, 4, 14, 13, 7, 4, 8, 4, 4, 0, 10, 9, 8, 4, 13, 4],
+                [6, 7, 15, 6, 4, 9, 12, 5, 9, 6, 10, 0, 1, 3, 7, 3, 10],
+                [4, 5, 14, 7, 6, 7, 10, 4, 8, 5, 9, 1, 0, 2, 6, 4, 8],
+                [4, 8, 13, 9, 8, 7, 10, 3, 7, 4, 8, 3, 2, 0, 4, 5, 6],
+                [5, 12, 9, 14, 12, 6, 6, 7, 3, 3, 4, 7, 6, 4, 0, 9, 2],
+                [9, 10, 18, 6, 8, 12, 15, 8, 13, 9, 13, 3, 4, 5, 9, 0, 9],
+                [7, 14, 9, 16, 14, 8, 5, 10, 6, 5, 4, 10, 8, 6, 2, 9, 0],
+              ]
+    # According to our problem, the first parameter will be zero only
+    data['time_windows'] = [
+                (0, 60),  # depot
+                (0, 30),  # 1
+                (0, 40),  # 2
+                (0, 50),  # 3
+                (0, 30),  # 4
+                (0, 40),  # 5
+                (0, 60),  # 6
+                (0, 50),  # 7
+                (0, 50),  # 8
+                (0, 30),  # 9
+                (0, 40),  # 10
+                (0, 15),  # 11
+                (0, 5),  # 12
+                (0, 10),  # 13
+                (0, 8),  # 14
+                (0, 15),  # 15
+                (0, 15),  # 16
+              ]
+    data['demands'] = [0, 1, 1, 2, 4, 2, 4, 8, 3, 1, 2, 1, 2, 4, 4, 5, 5]
+    data['vehicle_capacities'] = [15, 15]
+    data['num_vehicles'] = 2
+    data['depot'] = 0
+    return data
+
+def print_solution(data, manager, routing, assignment, time_callback):
+    """Prints assignment on console."""
+    print(f'Objective: {assignment.ObjectiveValue()}')
+    # Display dropped nodes.
+    dropped_nodes = 'Dropped nodes:'
+    for node in range(routing.Size()):
+        if routing.IsStart(node) or routing.IsEnd(node):
+            continue
+        if assignment.Value(routing.NextVar(node)) == node:
+            dropped_nodes += ' {}'.format(manager.IndexToNode(node))
+    print(dropped_nodes)
+    # Display routes
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = assignment.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total Distance of all routes: {}m'.format(total_distance))
+    print('Total Load of all routes: {}'.format(total_load))
+
+def cvrptw_with_dropped_locations():
+    # This function will be used to calculate the routes with dropped locations
+    
+    # Instantiate the data problem
+    data = create_data_model()
+
+    # Create the routing index manager
+    manager = pywrapcp.RoutingIndexManager(len(data['time_matrix']),data['num_vehicles'], data['depot'])
+    
+    # Create Routing Model
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def time_callback(from_index, to_index):
+        """Returns the travel time between the two nodes."""
+        # Convert from routing variable Index to time matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['time_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(time_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
+        True,  # start cumul to zero
+        'Capacity')
+    
+    # Allow to drop nodes.
+    penalty = 1000
+    for node in range(1, len(data['time_matrix'])):
+        routing.AddDisjunction([manager.NodeToIndex(node)], penalty)
+
+    # Add Time Windows constraint.
+    time = 'Time'
+    # routing.AddDimension(transit_callback_index,
+    #     30,  # allow waiting time
+    #     30,  # maximum time per vehicle
+    #     False,  # Don't force start cumul to zero.
+    #     time)
+    time_dimension = routing.GetDimensionOrDie(time)
+    
+    # Add time window constraints for each location except depot.
+    for location_idx, time_window in enumerate(data['time_windows']):
+        if location_idx == 0:
+            continue
+        index = manager.NodeToIndex(location_idx)
+        time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.time_limit.FromSeconds(1)
+
+    # Solve the problem.
+    assignment = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if assignment:
+        print_solution(data, manager, routing, assignment, time_callback)
 
 @csrf_exempt
 def get_waypoint_to_coord(request):
